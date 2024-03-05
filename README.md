23d46a4fa91d5162536036058374e617168626ed
Jaguirre2001 <162341669+Jaguirre2001@users.noreply.github.com>
2024-03-05 15:26:39 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+![WhatsApp Image 2024-02-14 at 7 09 22 PM (1)](https://github.com/JorgeAAG/RegistroFacial/assets/151756124/f305cc32-93d2-4fab-b44f-70a3a7fdde75)
+![WhatsApp Image 2024-02-14 at 7 09 22 PM](https://github.com/JorgeAAG/RegistroFacial/assets/151756124/a1b14c03-3a15-4b6e-80a2-b8311e13451a)
+![WhatsApp Image 2024-02-14 at 7 09 21 PM](https://github.com/JorgeAAG/RegistroFacial/assets/151756124/1974f947-9932-4f84-a9b0-94b382a83cdb)
---
+++ app/src/main/java/com/example/machinelearning/MainActivity.java
@@ -0,0 +1,166 @@
+package com.example.machinelearning;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.machinelearning.ml.Rostros;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
+import java.util.List;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements OnSuccessListener<Text>, OnFailureListener {
+    public static int REQUEST_CAMERA = 111;
+    public static int REQUEST_GALLERY = 222;
+
+    Bitmap mSelectedImage;
+    ImageView mImageView;
+    TextView txtResults;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtResults = findViewById(R.id.txtresults);
+        mImageView = findViewById(R.id.image_view);
+
+
+        mImageView = findViewById(R.id.image_view);
+        if(checkSelfPermission(Manifest.permission.CAMERA) !=  PackageManager.PERMISSION_GRANTED)
+            requestPermissions(new String[]{Manifest.permission.CAMERA},100);
+
+
+    }
+
+    @Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (resultCode == RESULT_OK && null != data) {
+        try {
+            if (requestCode == REQUEST_CAMERA)
+                mSelectedImage = (Bitmap) data.getExtras().get("data");
+            else
+                 mSelectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+
+            mImageView.setImageBitmap(mSelectedImage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
+    public void abrirGaleria (View view){
+        Intent i = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, REQUEST_GALLERY);
+    }
+
+    public void abrirCamera (View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    public void OCRfx(View v) {
+        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        recognizer.process(image)
+                .addOnSuccessListener(this)
+                .addOnFailureListener(this);
+    }
+
+    public void PersonalizedModel(View v){
+        try {
+            String[] etiquetas = {"Fernando", "Solange", "Domenica", "Leandro", "Holger"};
+            Rostros model = Rostros.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
+            Rostros.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+            txtResults.setText(obtenerEtiquetayProbabilidad(etiquetas, outputFeature0.getFloatArray()));
+            model.close();
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
+        }
+    }
+
+    public ByteBuffer convertirImagenATensorBuffer(Bitmap mSelectedImage){
+        Bitmap imagen = Bitmap.createScaledBitmap(mSelectedImage, 224, 224, true);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3);
+        byteBuffer.order(ByteOrder.nativeOrder());
+        int[] intValues = new int[224 * 224];
+        imagen.getPixels(intValues, 0, imagen.getWidth(), 0, 0, imagen.getWidth(), imagen.getHeight());
+        int pixel = 0;
+        for(int i = 0; i < imagen.getHeight(); i ++){
+            for(int j = 0; j < imagen.getWidth(); j++){
+                int val = intValues[pixel++]; // RGB
+                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+            }
+        }
+        return byteBuffer;
+    }
+    public String obtenerEtiquetayProbabilidad(String[] etiquetas, float[] probabilidades){
+        float valorMayor=Float.MIN_VALUE;
+        int pos=-1;
+        for (int i = 0; i < probabilidades.length; i++) {
+            if (probabilidades[i] > valorMayor) {
+                valorMayor = probabilidades[i];
+                pos = i;
+            }
+        }
+        return "Predicción: " + etiquetas[pos] + ", Probabilidad: " +
+                (new DecimalFormat("0.00").format(probabilidades[pos] * 100)) + "%";
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+
+    @Override
+    public void onSuccess(Text text) {
+            List<Text.TextBlock> blocks = text.getTextBlocks();
+            String resultados="";
+            if (blocks.size() == 0) {
+                resultados = "No hay Texto";
+            }else{
+                for (int i = 0; i < blocks.size(); i++) {
+                    List<Text.Line> lines = blocks.get(i).getLines();
+                    for (int j = 0; j < lines.size(); j++) {
+                        List<Text.Element> elements = lines.get(j).getElements();
+                        for (int k = 0; k < elements.size(); k++) {
+                            resultados = resultados + elements.get(k).getText() + " ";
+                        }
+                    }
+                }
+                resultados=resultados + "\n";
+            }
+            txtResults.setText(resultados);
+        }
+
+    }
